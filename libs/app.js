8a040a11652752b1b2fd5ba5e005032bcf71a667
Anthony Stabile <Anthony Stabile>
2015-12-03 08:41:49 -0600

    fixed scope.page readability, deleted some junk js code

--- libs/app.js
+++ libs/app.js
@@ -1,26 +1,38 @@
 var app = angular.module("ang", []);
 
 app.controller('MainController', ['$scope', function($scope) {
-    $scope.thing = 'the button is white';
-    $scope.stuff = "just kidding!";
-    $scope.theChanger = false;
-    $scope.buttColor = "white";
-    $scope.ropemaker = function ($scope) {
-        this.thing = this.stuff;
-        this.buttColor = "blue";
-        this.theChanger = true;
-    } 
-
- 
    $scope.page = [
-        { entry: "Home", url: "pages/home.html", head: "Home", picture: "", shoppable: false},
-        { entry: "Shopping", url: "pages/shopping.html", head: "Shopping", picture: "", shoppable: false},
-        { entry: 'Mice', url: "pages/shopitem.html", head: "Shopping > Mice", picture: "img/g502.png", shoppable: true},
-        { entry: "Keyboards", url: "pages/shopitem.html", head: "Shopping > Keyboards", picture: "img/blackwid.png", shoppable: true},
-        { entry: "Headsets", url: "pages/shopitem.html", head: "Shopping > Headsets", picture: "img/g430.png", shoppable: true},
-        { entry: "Monitors", url: "pages/shopitem.html", head: "Shopping > Monitors", picture: "", shoppable: true}
+        {   entry: "Home", 
+            url: "pages/home.html", 
+            head: "Home", 
+            picture: "", 
+            shoppable: false },
+        {   entry: "Shopping", 
+            url: "pages/shopping.html", 
+            head: "Shopping", 
+            picture: "", 
+            shoppable: false },
+        {   entry: 'Mice', 
+            url: "pages/shopitem.html", 
+            head: "Shopping > Mice", 
+            picture: "img/g502.png", 
+            shoppable: true },
+        {   entry: "Keyboards", 
+            url: "pages/shopitem.html", 
+            head: "Shopping > Keyboards", 
+            picture: "img/blackwid.png", 
+            shoppable: true },
+        {   entry: "Headsets", 
+            url: "pages/shopitem.html", 
+            head: "Shopping > Headsets", 
+            picture: "img/g430.png", 
+            shoppable: true },
+        {   entry: "Monitors", 
+            url: "pages/shopitem.html", 
+            head: "Shopping > Monitors", 
+            picture: "", 
+            shoppable: true }
     ];
-
     $scope.menuShow = false;
     $scope.menuText = false;
     $scope.menuClick = function () {
@@ -33,11 +45,8 @@ app.controller('MainController', ['$scope', function($scope) {
         $scope.menuShow = false;    
     };
     $scope.headerClick = function (activePage) {
-        if (activePage.shoppable == true) {
-            $scope.activePage = $scope.page[1];
-        };
+        if (activePage.shoppable == true) { $scope.activePage = $scope.page[1]; };
     };
-
     $scope.activePage = $scope.page[0];
     $scope.activeShop = null;
     $scope.shopSel = function (index) {
